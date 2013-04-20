a71e15d285620b9eb0e3facc347315fb4fc06693
Andrew <andrew_jones@outlook.com>
2013-04-20 15:14:44 +0100

    Start of crime app

---
+++ UK-Crime-App/UK-Crime-App/ItemsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UK_Crime_App.DataModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UK_Crime_App
+{
+    public sealed partial class ItemsPage
+    {
+        public ItemsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            //var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
+            //this.DefaultViewModel["Items"] = sampleDataGroups;
+        }
+
+        void ItemViewItemClick(object sender, ItemClickEventArgs e)
+        {
+            var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
+            this.Frame.Navigate(typeof(SplitPage), groupId);
+        }
+
+        private void BtnSearchClick(object sender, RoutedEventArgs e)
+        {
+            //txtPostcode.Text
+        }
+
+    }
+}
